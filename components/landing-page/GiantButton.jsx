4f5356c0b1bb7ab544ca2a2462a43ff2243af54e
Jon-Michael Gelua <geluajm@gmail.com>
2022-02-03 09:36:48 -0500

    refactor giant button

--- components/landing-page/GiantButton.jsx
+++ components/landing-page/GiantButton.jsx
@@ -5,12 +5,11 @@ import Link from "next/link";
 const GiantButton = () => {
   return (
     <Link href="/">
-      <button
-        className="
-        h-1/4 min-h-20 bg-yellow-500 mt-auto overflow-hidden active:scale-95
+      <a
+        className="h-1/4 min-h-20 bg-yellow-500 mt-auto overflow-hidden active:scale-95
         group flex justify-center items-center ease-in-out duration-500"
       >
-        <div
+        <button
           className="px-12 lg:px-20 py-12 space-x-5 w-full h-full 
           flex justify-between items-center border-t box-border whitespace-nowrap"
         >
@@ -24,13 +23,12 @@ const GiantButton = () => {
             className="
             z-0 text-lg lg:text-2xl bg-white text-black ease-in-out duration-1000 rounded-full p-4
             group-hover:text-stone-200 group-hover:bg-white group-hover:-rotate-45
-            group-hover:scale-[200]
-          "
+            group-hover:scale-[200]"
           >
             <BsArrowDownRight />
           </div>
-        </div>
-      </button>
+        </button>
+      </a>
     </Link>
   );
 };
